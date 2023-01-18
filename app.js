b1057b43787f0d4d3dafcad996d8972e6d313b40
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-18 20:02:01 +0100

    generating quares

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const horizontalCount = 16
-const verticalCount = 16
-const totalCount = horizontalCount * verticalCount
+const horizontalCount = 16;
+const verticalCount = 16;
+const totalCount = horizontalCount * verticalCount;
+const container = document.querySelector(".container")
+
+const createDiv = (total) => {
+    for(let i = 0; i <= total ;i++){
+        let div = document.createElement("div")
+        div.classList.add("square")
+        container.appendChild(div)
+    }
+};
+createDiv(totalCount)
