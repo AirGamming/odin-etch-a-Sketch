791d2d5b0ca91b792dfd10e1a9c768c679a7d7e5
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-19 11:04:24 +0100

    working main function

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const sizeCount = 16;
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 const createDiv = (size) => {
     const totalCount = sizeCount * sizeCount;
-    container.style.gridTemplateColumns = `repeat(${size},1fr)`; 
-    for(let i = 1; i <= totalCount ;i++){
-        let div = document.createElement("div");
-        div.classList.add("square");
+    container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    for (let i = 1; i <= totalCount; i++) {
+        let div = document.createElement('div');
+        div.classList.add('square');
         container.appendChild(div);
-    };
-    let table = document.querySelectorAll(".square");
-    table.forEach(e =>{
-        e.addEventListener("mouseover",() =>{
-            e.classList.add = "black"
-        })
+    }
+    let table = document.querySelectorAll('.square');
+    table.forEach((e) => {
+        e.addEventListener('mouseover', (el) => {
+            if (el.target.classList == "square"){
+                el.target.classList = "black"
+            }else{
+                el.target.classList = "square"
+            }
+            
+            
+            console.log(el.target.classList);
+        });
     });
 };
-createDiv(sizeCount)
+
+createDiv(sizeCount);
